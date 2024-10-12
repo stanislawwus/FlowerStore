1589ce42229b0d4c1739742a8c56fbed89ff22c0
stanislawwus <vus.pn@ucu@edu.ua>
2024-10-12 22:53:30 +0300

    add tests

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -1,10 +1,10 @@
 package flower.store;
 
+import java.util.Random;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
-import java.util.Random;
 
 public class FlowerBucketTest {
     private static final Random RANDOM_GENERATOR = new Random();
@@ -23,9 +23,28 @@ public class FlowerBucketTest {
         int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
         int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
         Flower flower = new Rose();
-        flower.setPrice(10);
+        flower.setPrice(price);
         FlowerPack flowerPack = new FlowerPack(flower, quantity);
         flowerBucket.add(flowerPack);
         Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
+    }
+
+    @Test
+    public void testAnother() {
+        flowerBucket = new FlowerBucket();
+        int quantityCham = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
+        int quantityTulip = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
+
+        Chamomile chamomile = new Chamomile();
+        Tulip tulip = new Tulip();
+
+        FlowerPack flowerPackRose = new FlowerPack(chamomile, quantityCham);
+        FlowerPack flowerPackTulip = new FlowerPack(tulip, quantityTulip);
+
+        flowerBucket.add(flowerPackRose);
+        flowerBucket.add(flowerPackTulip);
+
+        Assertions.assertEquals(quantityCham * chamomile.getPrice()
+            + quantityTulip * tulip.getPrice(), flowerBucket.getPrice());
     }
 }
